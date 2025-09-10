b9268646e41b87bc4bdd8331ed47baf46130b3a7
Rose7230 <Rose7230@users.noreply.github.com>
2025-09-10 02:05:54 -0400

    Add named arguments and run script
    Added default value for result_column parameter.
    Updated print_fires.py to use named arguments.
    Created run.sh script to run the demo.

--- print_fires.py
+++ print_fires.py
@@ -5,6 +5,6 @@ country_column = 0  # Area column (countries are in column 0, not 1)
 fires_column = 4
 file_name = 'Agrofood_co2_emission.csv'
 
-# call get_column with the right parameters
-fires = get_column(file_name, country_column, country, fires_column)
+# call get_column using named argument for result_column
+fires = get_column(file_name, country_column, country, result_column=fires_column)
 print(fires)
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Script to run the print_fires.py demo
+
+echo "Running fire data analysis..."
+python print_fires.py
+echo "Done!"
