c57d7979b2430057d10a7d465f8e55b3aecaea62
Rose7230 <Rose7230@users.noreply.github.com>
2025-09-10 02:23:41 -0400

    Fix robot-like comments
    Made the comments sound more like a student wrote them instead of formal technical language

--- print_fires.py
+++ print_fires.py
@@ -1,10 +1,10 @@
 from my_utils import get_column
 
 country = 'United States of America'
-country_column = 0  # Area column (countries are in column 0, not 1)
+country_column = 0  # countries are in column 0, not 1
 fires_column = 4
 file_name = 'Agrofood_co2_emission.csv'
 
-# call get_column using named argument for result_column
+# get the fire data for this country
 fires = get_column(file_name, country_column, country, result_column=fires_column)
 print(fires)
